7449d6d1eac42a16d4c1ddfbbda9096c861e1a16
David-Wobrock <david.wobrock@gmail.com>
2016-04-23 22:05:50 +0200

    add dao to retrieve object

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 "use strict";
 
-var express = require('express');
-var path    = require('path');
-var config 	= require(path.join(__dirname, 'config.js'));
-var dao     = require(path.join(__dirname, 'dao.js'));
+var express       = require('express');
+var path          = require('path');
+var config 	      = require(path.join(__dirname, 'config.js'));
+var dao           = require(path.join(__dirname, 'dao','dao.js'));
 // var globe = require('./helpers/globe.js');
 
 var app = express();
@@ -19,14 +19,13 @@ app.use(function(req, res, next) {
 var findInfoById = function (index, callback) {
   // Perform database query that calls callback when it's done
   // This is our SpaceLocation database
-  var spaceLocation = dao.getSpaceLocationFromDB(index);
-  if (!spaceLocation)
-    return callback(new Error(
-      'No id matching '
-       + index
-      )
-    );
-  return callback(null, SpaceLocation[index]);
+  dao.getSpaceLocationFromDB(index, function(location) {
+    if (!location) {
+      return callback(new Error('No id matching '+ index), null);
+    }
+    return callback(null, location);
+  });
+  
 };
 
 app.get('/', function (req, res) {
@@ -37,10 +36,13 @@ app.get('/:id', function (req, res) {
   var index = req.params.id;
   findInfoById(index, function(error, location) {
     if (error) { /* Get Error and display it */}
-
+    
     //TODO: Fill index file with data object location
+    // location  is accessible for information 
     res.sendFile(path.join(__dirname+'/views/index.html'));
   });
+    
+  
 });
 
 var server = app.listen(config.port, function () {
--- dao/dao.js
+++ dao/dao.js
@@ -1,6 +1,6 @@
 var mysql   = require('mysql');
 var path    = require('path');
-var config 	= require(path.join(__dirname, 'config.js'));
+var config 	= require(path.join(__dirname, '..', 'config.js'));
 
 var dao = {};
 
@@ -12,10 +12,10 @@ var pool = mysql.createPool({
 	database : config.dbname
 });
 
-dao.getSpaceLocationFromDB = function getSpaceLocationFromDB (id) {
+dao.getSpaceLocationFromDB = function getSpaceLocationFromDB (id, callback) {
 	if (isNaN(parseInt(id))) {
 		console.log("id is not a number");
-		return null;
+		return callback(null);
 	}
 	
 	pool.getConnection(function(err, connection) {
@@ -24,23 +24,30 @@ dao.getSpaceLocationFromDB = function getSpaceLocationFromDB (id) {
 				connection.release();
 			}
 			console.log("Error getting SpaceLocation " + err);
-			return null;
+		return callback(null);
 		}
 		
 		console.log('Connected as id: ' + connection.threadId);
 		// Careful with injection!
-		connection.query("SELECT * FROM SpaceLocation WHERE id="+id, function(err, rows) {
+		connection.query("SELECT * FROM SpaceLocation sl, EarthPosition ep WHERE sl.id="+id + " AND sl.idEarthPosition=ep.id;", function(err, rows) {
 			connection.release();
 			if (err) {
 				console.log("Error after select query: " + err);
-				return null;
+		return callback(null);
 			};
 			if (rows.length == 0) {
 				console.log("No row with id " + id + " found.");
-				return null;
+		return callback(null);
 			}
-			console.log("Requested row: "+ rows[0]);
-			return rows[0];
+			spaceRow = rows[0];
+			spaceLocation = {
+				date: spaceRow['moment'],
+				lat: spaceRow['latitude'],
+				long: spaceRow['longitude'],
+				altitude: spaceRow['altitude']
+			};
+			console.log(spaceLocation);
+			return callback(spaceLocation);
 		});
 	});
 };
