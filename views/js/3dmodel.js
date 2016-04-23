abf342ba87eb14a3eb57c032fe180ecdcfc6adc6
David-Wobrock <david.wobrock@gmail.com>
2016-04-23 23:53:14 +0200

    add real size of sun

--- views/js/3dmodel.js
+++ views/js/3dmodel.js
@@ -3,20 +3,25 @@ var createSpaceScene = function() {
 	scene = new BABYLON.Scene(engine);
 	
     //Adding a light
-    var light = new BABYLON.PointLight("Omni", new BABYLON.Vector3(20, 20, 100), scene);
+    var light = new BABYLON.PointLight("Omni", new BABYLON.Vector3(0, 0, 0), scene);
 
-	camera = new BABYLON.ArcRotateCamera("Camera", -1.5, 1.3, 500, BABYLON.Vector3.Zero(), scene);
+    // alpha = longitude (rad)
+    // beta = latitude (rad)
+    // radius = altitude/height
+    var alpha = 0;
+    var beta = 0;
+    var altitude = 1000;
+    var cameraTarget = new BABYLON.Vector3(tweetPos.x, tweetPos.y, tweetPos.z);
+	camera = new BABYLON.ArcRotateCamera("Camera", alpha, beta, altitude, cameraTarget, scene);
     camera.attachControl(canvas, false);
-    camera.upperRadiusLimit = 600;
-    camera.lowerRadiusLimit = 200;
-	
-    // target the camera to scene origin
-    camera.setTarget(BABYLON.Vector3.Zero());
+    /*camera.upperRadiusLimit = 1000000;
+    camera.lowerRadiusLimit = 800000;*/
 
     // attach the camera to the canvas
     //camera.attachControl(canvas, false);
 	
 	var earth = BABYLON.Mesh.CreateSphere("earth", 16, 80, scene);
+    earth.position = new BABYLON.Vector3(earthPos.x, earthPos.y, earthPos.z);
     var earthMaterial = new BABYLON.StandardMaterial("earthMaterial", scene);
     earthMaterial.diffuseTexture = new BABYLON.Texture('views/images/earth.jpg', scene);
     earthMaterial.diffuseColor = new BABYLON.Color3(0.3, 0.3, 0.3);
@@ -28,8 +33,8 @@ var createSpaceScene = function() {
     // position and scale
     godrays.mesh.material.diffuseTexture = new BABYLON.Texture('views/images/sun.png', scene, true, false, BABYLON.Texture.BILINEAR_SAMPLINGMODE);
     godrays.mesh.material.diffuseTexture.hasAlpha = true;
-    godrays.mesh.position = new BABYLON.Vector3(-150, 150, 150);
-    godrays.mesh.scaling = new BABYLON.Vector3(35, 35, 35);
+    godrays.mesh.position = new BABYLON.Vector3(0, 0, 0);
+    godrays.mesh.scaling = new BABYLON.Vector3(695.7, 695.7, 695.7);
 
     light.position = godrays.mesh.position;
 
