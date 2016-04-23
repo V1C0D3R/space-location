2e3968f16a950e871f35ace5c1c0202ae8b06508
V1C0D3R <V1C0D3R@gmail.com>
2016-04-23 23:11:18 +0200

    Add sun

--- views/js/3dmodel.js
+++ views/js/3dmodel.js
@@ -2,6 +2,9 @@ var createSpaceScene = function() {
 	
 	scene = new BABYLON.Scene(engine);
 	
+    //Adding a light
+    var light = new BABYLON.PointLight("Omni", new BABYLON.Vector3(20, 20, 100), scene);
+
 	camera = new BABYLON.ArcRotateCamera("Camera", -1.5, 1.3, 500, BABYLON.Vector3.Zero(), scene);
     camera.attachControl(canvas, false);
     camera.upperRadiusLimit = 600;
@@ -16,10 +19,19 @@ var createSpaceScene = function() {
 	var earth = BABYLON.Mesh.CreateSphere("earth", 16, 80, scene);
     var earthMaterial = new BABYLON.StandardMaterial("earthMaterial", scene);
     earthMaterial.diffuseTexture = new BABYLON.Texture('views/images/earth.jpg', scene);
-    earthMaterial.diffuseColor = new BABYLON.Color3(0.8, 0.8, 0.8);
+    earthMaterial.diffuseColor = new BABYLON.Color3(0.3, 0.3, 0.3);
     earthMaterial.emissiveColor = BABYLON.Color3.White();
 
-    var sun = new BABYLON.PointLight("sun", new BABYLON.Vector3(50, 50, 30), scene);
+    var godrays = new BABYLON.VolumetricLightScatteringPostProcess('godrays', 1.0, camera, null, 100, BABYLON.Texture.BILINEAR_SAMPLINGMODE, engine, false);
+
+    // By default it uses a billboard to render the sun, just apply the desired texture
+    // position and scale
+    godrays.mesh.material.diffuseTexture = new BABYLON.Texture('views/images/sun.png', scene, true, false, BABYLON.Texture.BILINEAR_SAMPLINGMODE);
+    godrays.mesh.material.diffuseTexture.hasAlpha = true;
+    godrays.mesh.position = new BABYLON.Vector3(-150, 150, 150);
+    godrays.mesh.scaling = new BABYLON.Vector3(350, 350, 350);
+
+    light.position = godrays.mesh.position;
 
 	return scene;
 };
